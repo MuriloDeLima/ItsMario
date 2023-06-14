d177c7b0ee6c2ddd1398e6e6461b7596a91e9485
Murilo <martinsmurilo980@gmail.com>
2023-06-13 21:24:22 -0300

    Finalizado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Log posX personagem direita e esquerda 
+// lógica de posicionamento do personagem direita e esquerda
 // var posX = 200
 // var layoutWidth = $("#layout").width()
 // $("#playerMario").css({ "left": posX + "px" })
@@ -27,6 +27,8 @@
 
 //     }
 // )
+
+
 var posX = 200
 var layoutWidth = $("#layout").width()
 $("#playerMario").css({ "left": posX + "px" })
@@ -36,29 +38,28 @@ $("#right").click(
         $("#mario").attr('src', '../assets/mario.gif')
 
         if (posX < layoutWidth - 150) {
-            posX += 100
+            posX += 100 
             $("#playerMario").animate({ left: posX + "px" }, { duration: 200 })
         } else {
-            alert("vc bateu na parede")
+            alert("Parece que este é o FIM <->")
         }
 
     }
 )
 $("#left").click(
     function () {
-        $("#mario").attr('src', '../assets/marioLeft2.gif')
+        $("#mario").attr('src', '../assets/marioLeft.gif')
         if (posX > 0) {
             posX -= 100
             $("#playerMario").animate({ left: posX + "px" }, { duration: 200 })
         } else {
-            alert("vc bateu na parede")
+            alert("Parece que este é o FIM <->")
         }
 
     }
 )
 
 var posY = 45
-
 
 $("#playerMario").css({ "bottom": posY + "px" })
 $("#jump").click(
